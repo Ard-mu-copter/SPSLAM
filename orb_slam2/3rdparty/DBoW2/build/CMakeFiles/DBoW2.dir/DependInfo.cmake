a698b4913b9b262c25f26fdf62998c7a828b44d3
hezhijian <eehezhijian@ust.hk>
2021-03-01 20:19:01 +0800

    add lib and close loop detect code

---
+++ orb_slam2/3rdparty/DBoW2/build/CMakeFiles/DBoW2.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hzj/paper/IROS2021/WORKSPACE/src/sp_orb_slam/orb_slam2/3rdparty/DBoW2/src/BowVector.cpp" "/home/hzj/paper/IROS2021/WORKSPACE/src/sp_orb_slam/orb_slam2/3rdparty/DBoW2/build/CMakeFiles/DBoW2.dir/src/BowVector.cpp.o"
+  "/home/hzj/paper/IROS2021/WORKSPACE/src/sp_orb_slam/orb_slam2/3rdparty/DBoW2/src/FBrief.cpp" "/home/hzj/paper/IROS2021/WORKSPACE/src/sp_orb_slam/orb_slam2/3rdparty/DBoW2/build/CMakeFiles/DBoW2.dir/src/FBrief.cpp.o"
+  "/home/hzj/paper/IROS2021/WORKSPACE/src/sp_orb_slam/orb_slam2/3rdparty/DBoW2/src/FORB.cpp" "/home/hzj/paper/IROS2021/WORKSPACE/src/sp_orb_slam/orb_slam2/3rdparty/DBoW2/build/CMakeFiles/DBoW2.dir/src/FORB.cpp.o"
+  "/home/hzj/paper/IROS2021/WORKSPACE/src/sp_orb_slam/orb_slam2/3rdparty/DBoW2/src/FeatureVector.cpp" "/home/hzj/paper/IROS2021/WORKSPACE/src/sp_orb_slam/orb_slam2/3rdparty/DBoW2/build/CMakeFiles/DBoW2.dir/src/FeatureVector.cpp.o"
+  "/home/hzj/paper/IROS2021/WORKSPACE/src/sp_orb_slam/orb_slam2/3rdparty/DBoW2/src/QueryResults.cpp" "/home/hzj/paper/IROS2021/WORKSPACE/src/sp_orb_slam/orb_slam2/3rdparty/DBoW2/build/CMakeFiles/DBoW2.dir/src/QueryResults.cpp.o"
+  "/home/hzj/paper/IROS2021/WORKSPACE/src/sp_orb_slam/orb_slam2/3rdparty/DBoW2/src/ScoringObject.cpp" "/home/hzj/paper/IROS2021/WORKSPACE/src/sp_orb_slam/orb_slam2/3rdparty/DBoW2/build/CMakeFiles/DBoW2.dir/src/ScoringObject.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DBoW2_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include/DBoW2"
+  "../include"
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ orb_slam2/3rdparty/DBoW2/build/CMakeFiles/demo.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hzj/paper/IROS2021/WORKSPACE/src/sp_orb_slam/orb_slam2/3rdparty/DBoW2/demo/demo.cpp" "/home/hzj/paper/IROS2021/WORKSPACE/src/sp_orb_slam/orb_slam2/3rdparty/DBoW2/build/CMakeFiles/demo.dir/demo/demo.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include/DBoW2"
+  "../include"
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/hzj/paper/IROS2021/WORKSPACE/src/sp_orb_slam/orb_slam2/3rdparty/DBoW2/build/CMakeFiles/DBoW2.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
